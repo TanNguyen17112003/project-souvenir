e3b61c1f009d0d24f82bd1fcf693e9e342679438
MinhTuei <ghetueyh@gmail.com>
2023-07-18 17:47:26 +0700

    Add Map in Footer

--- client/src/DynamicComponents/LatestItems.js
+++ client/src/DynamicComponents/LatestItems.js
@@ -1,29 +1,36 @@
-import React from 'react';
-import { FaStar } from 'react-icons/fa';
+import React from "react";
+import { FaStar } from "react-icons/fa";
 
-function LatestItems({items}) {
-  return (
-    <div className='grid grid-cols-4 gap-[20px] place-items-center p-10'>
-          {items.map((item) => (
-            <div key={item.id} className='cursor-pointer hover:border-slate-400 flex flex-col justify-center items-center border-2 border-solid border-slate-300 w-[250px]'>
-              <div className='p-[10px]'>
-                <img src={item.url} alt="" className='h-[80%] mb-[10px]'/>
-                <h3 className='font-bold uppercase mb-[20px]'>{item.name}</h3>
-                <div className='flex'>
-                    <FaStar className='text-yellow-300'/>
-                    <FaStar className='text-yellow-300'/>
-                    <FaStar className='text-yellow-300'/>
-                    <FaStar className='text-yellow-300'/>
-                    <FaStar className='text-yellow-300'/>
+function LatestItems({ items }) {
+    return (
+        <div className="grid grid-cols-4 gap-[20px] place-items-center p-10">
+            {items.map((item) => (
+                <div
+                    key={item.id}
+                    className="cursor-pointer hover:border-slate-400 flex flex-col justify-center items-center border-2 border-solid border-slate-300 w-[250px]"
+                >
+                    <div className="p-[10px]">
+                        <img
+                            src={item.url}
+                            alt=""
+                            className="h-[80%] mb-[10px]"
+                        />
+                        <h3 className="font-bold uppercase mb-[20px]">
+                            {item.name}
+                        </h3>
+                        <div className="flex">
+                            <FaStar className="text-yellow-300" />
+                            <FaStar className="text-yellow-300" />
+                            <FaStar className="text-yellow-300" />
+                            <FaStar className="text-yellow-300" />
+                            <FaStar className="text-yellow-300" />
+                        </div>
+                        <p className="mt-[10px]">{item.price}</p>
+                    </div>
                 </div>
-                <p
-                    className='mt-[10px]'
-                >{item.price}</p>
-              </div>
-            </div>
-          ))}
-    </div>
-  )
+            ))}
+        </div>
+    );
 }
 
-export default LatestItems
+export default LatestItems;
--- client/src/FixedComponents/Footer.jsx
+++ client/src/FixedComponents/Footer.jsx
@@ -1,75 +1,101 @@
-import React from 'react';
-import { TbCopyrightFilled } from 'react-icons/tb';
-import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare, FaLinkedin } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { TbCopyrightFilled } from "react-icons/tb";
+import {
+    FaFacebookSquare,
+    FaInstagramSquare,
+    FaTwitterSquare,
+    FaLinkedin,
+    FaTiktok,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+import { useMemo } from "react";
+function Footer() {
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    });
+    const center = useMemo(() => {
+        return { lat: 10.88081, lng: 106.80534 };
+    }, []);
+    if (!isLoaded) return "Loading...";
+    else {
+        return (
+            <div className="bg-[#26282b] flex justify-around   py-10 px-0">
+                <div className="flex flex-col">
+                    <h3 className="uppercase text-white">
+                        công ty cổ phần tập đoàn TTV
+                    </h3>
+                    <span className="text-neutral-50">
+                        Trụ sở chính: Số 50 Đường Lê Cao Lãng, Phường Phú Thạnh,{" "}
+                        <br /> Quận Tân Phú, Thành phố Hồ Chí Minh
+                    </span>
+                    <span className="text-neutral-50">
+                        GPĐK: 0102721191 cấp ngày 14/07/2023
+                    </span>
+                    <span className="text-neutral-50">
+                        ĐT: 0862898859 Email: duytan17112003@gmail.com
+                    </span>
+                    <span className="uppercase text-white mt-5">
+                        <TbCopyrightFilled className="inline" /> Copyright All
+                        rights reserved
+                    </span>
+                </div>
 
-function Footer() {  
-  return (
-    <div
-      className='bg-[#26282b] flex justify-around py-10 px-0'
-    >
-      <div 
-        className="flex flex-col"
-      >
-        <h3
-          className='uppercase text-white'
-        >
-          công ty cổ phần tập đoàn TTV
-        </h3>
-        <span
-          className='text-neutral-50'
-        >Trụ sở chính: Số 50 Đường Lê Cao Lãng, Phường Phú Thạnh, <br/> Quận Tân Phú, Thành phố Hồ Chí Minh</span>
-        <span
-          className='text-neutral-50'
-        >GPĐK: 0102721191 cấp ngày 14/07/2023</span>
-        <span
-          className='text-neutral-50'
-        >ĐT: 0862898859 Email: duytan17112003@gmail.com</span>
-      </div>
+                <div className="footer-reserved">
+                    <h3 className="uppercase text-white">
+                        Các kênh thông tin liên lạc
+                    </h3>
 
-      <div className="footer-reserved">
-        <h4
-          className='uppercase text-white'
-        >
-          <TbCopyrightFilled
-            className='inline'
-          /> Copyright All rights reserved
-        </h4>
-        <div 
-          className="mt-5 flex"
-        >
-          <Link to="https://www.facebook.com/">
-            <FaFacebookSquare 
-              size='2rem'
-              color='#fff'
-              className='hover:bg-indigo-600 hover:text-white'
-            />
-          </Link>
-          <Link to="https://www.instagram.com/">
-            <FaInstagramSquare 
-              size='2rem'
-              color='#fff'
-              className='hover:bg-indigo-600 hover:text-white'
-            />
-          </Link>
-          <Link to="https://twitter.com/home">
-            <FaTwitterSquare 
-              size='2rem'
-              color='#fff'
-              className='hover:bg-indigo-600 hover:text-white'
-            />
-          </Link>
-          <Link to="https://www.linkedin.com/feed/">
-            <FaLinkedin
-              size='2rem'
-              color='#fff'
-              className='hover:bg-indigo-600 hover:text-white'
-            />
-          </Link>  
-        </div>
-      </div>
-    </div>
-  )
+                    <div className="mt-5 ml-10 flex flex-col gap-y-2 h-(100%-20)">
+                        <Link to="https://www.facebook.com/">
+                            <FaFacebookSquare
+                                size="2rem"
+                                color="#fff"
+                                className="hover:bg-indigo-600 hover:text-white"
+                            />
+                        </Link>
+                        <Link to="https://www.instagram.com/">
+                            <FaInstagramSquare
+                                size="2rem"
+                                color="#fff"
+                                className="hover:bg-indigo-600 hover:text-white"
+                            />
+                        </Link>
+                        <Link to="https://www.tiktok.com/vi-VN/">
+                            <FaTiktok
+                                size="2rem"
+                                color="#fff"
+                                className="hover:bg-indigo-600 hover:text-white"
+                            />
+                        </Link>
+                        <Link to="https://twitter.com/home">
+                            <FaTwitterSquare
+                                size="2rem"
+                                color="#fff"
+                                className="hover:bg-indigo-600 hover:text-white"
+                            />
+                        </Link>
+                        <Link to="https://www.linkedin.com/feed/">
+                            <FaLinkedin
+                                size="2rem"
+                                color="#fff"
+                                className="hover:bg-indigo-600 hover:text-white"
+                            />
+                        </Link>
+                    </div>
+                </div>
+                <div className="map-container w-1/3 h-(100%-20) rounded-md overflow-hidden">
+                    <GoogleMap
+                        mapContainerStyle={{ width: "100%", height: "100%" }}
+                        center={center}
+                        zoom={15}
+                    >
+                        <MarkerF position={center} />
+                    </GoogleMap>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Footer;
